47d4ab37e01fafe584c53c1c7a4d69a734ad0ed8
Mike Brasher <mikbras@microsoft.com>
2021-03-08 17:41:16 +0000

    debug printf

--- lib/tst_test.c
+++ lib/tst_test.c
@@ -348,6 +348,8 @@ void tst_res_(const char *file, const int lineno, int ttype,
 	tst_vres_(file, lineno, ttype, fmt, va);
 	va_end(va);
 
+        fprintf(stderr, "XXXXXXXXX: %d TFAIL=%d\n", ttype, TFAIL);
+
         if (ttype == TFAIL)
         {
             fprintf(stderr, "%s(%u): crashing\n", __FILE__, __LINE__);
